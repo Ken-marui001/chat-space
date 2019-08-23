aa4469ad15ddc4bc699b70bdbb1a30a7761a9a02
ken-marui001 <noobgladiator001@gmail.com>
2019-08-23 16:54:03 +0900

    create models and add relations and add routing

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  has_mnany :users, through: :groups_users
+  has_many :groups_users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :messages, only: :index
   root 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, except: [:index, :destroy] do
+    resources :messages, only: [:index, :create]
+  end
 end
