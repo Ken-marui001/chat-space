4bd945693177c829e461b5ac03f409cbfad3b5fd
ken-marui001 <noobgladiator001@gmail.com>
2019-08-25 10:27:37 +0900

    add details for test list

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,24 +3,30 @@ describe Message do
   describe '#create' do
     describe 'can save' do
       it "is valid with text, imag, group and user" do
-
+        expect(build.(:message)).to be_valid
       end
       it "is valid with text, group and user" do
-
+        expect(build.(:message, image: nil)).to be_valid
       end
       it "is valid with imag, group and user" do
-
+        expect(build.(:message, text: nil)).to be_valid
       end
     end
     describe 'cannot save' do
       it "is invalid without text and image" do
-
+        message = build(:message, text: nil, image: nil)
+        message.valid?
+        expect(message.errors[:text]).to include('を入力してください')
       end
       it "is invalid without group_id" do
-
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group]).to include('を入力してください')
       end
       it "is invalid without group_id" do
-
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message.errors[:user]).to include('を入力してください')
       end
     end
   end
