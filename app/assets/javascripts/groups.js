0965b4f2294733f638e76cf314f56fa2647f1353
ken-marui001 <noobgladiator001@gmail.com>
2019-08-27 16:24:21 +0900

    add delete member button

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -18,6 +18,16 @@ $(function(){
     search_result_list.append(html);
   }
 
+  function addMemberHtml(id, name){
+    let html = `<div class='chat-group-user'>
+    <input name='group[user_ids][]' type='hidden' value='${id}'>
+    <p class='chat-group-user__name'>${name}</p>
+    <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+  </div>`
+
+    $('.chat-group-users.js-add-user').append(html);
+  }
+
   $('#user-search-field').on('keyup', function(){
     let input = $(this).val();
     
@@ -44,7 +54,15 @@ $(function(){
 
   $('#user-search-result').on('click', '.chat-group-user__btn--add', function(){
     let index = $('.chat-group-user__btn--add').index(this);
-    console.log(index);
-    $($('.chat-group-user')[index]).remove();
+    let name = $(this).attr('data-user-name');
+    let id = $(this).attr('data-user-id');
+
+    $($('.chat-group-user.clearfix')[index]).remove();
+
+    addMemberHtml(id, name);
+  });
+
+  $('.chat-group-users.js-add-user').on('click', '.js-remove-btn', function(){
+    
   });
 });
