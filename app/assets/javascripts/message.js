6c66998ecaaa00f92c91367eaa0c102588c5fcad
ken-marui001 <noobgladiator001@gmail.com>
2019-08-27 23:49:54 +0900

    defined reloadMessages function to reload automatics

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,6 +36,26 @@ $(function(){
       scrollTop: base[0].scrollHeight
     });
   }
+
+  let reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message').last().data('id');
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "../api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
   
   $('.form__box').on('submit', function(e){
     e.preventDefault();
